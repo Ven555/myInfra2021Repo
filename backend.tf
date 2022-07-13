13ab57bb411a862a3501e9c7a2470845381946e1
Ven555 <106283111+Ven555@users.noreply.github.com>
2022-07-12 21:57:25 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
-    key = "main"
-    region = "us-east-2"
+    bucket = "my-dev-tf-state-bucket"
+    key = "master"
+    region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
