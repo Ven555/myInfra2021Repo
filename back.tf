b2ac3e07db154c0042105861c6a9039db0c92a51
Ven555 <106283111+Ven555@users.noreply.github.com>
2022-07-12 22:15:51 -0500

    fixed naming of s3

--- back.tf
+++ back.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-dev-tf-state-bucket"
+    bucket = "my-dev-tf.state.bucket"
     key = "master"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
